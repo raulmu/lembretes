e21ea7a2eef5e9caa3e716f6bfab982929a70c45
Raul Uhlmann <raulmu@outlook.com>
2022-02-19 17:03:22 -0300

    redirectUri adjust

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,6 +4,6 @@ export const environment = {
   production: true,
   auth: {
     ...authConfig,
-    redirectUri: 'https://raulmu.github.io/lembretes/',
+    redirectUri: 'https://raul.seve.com.br/lembretes/',
   },
 };
