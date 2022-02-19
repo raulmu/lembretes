4e904e50c93f6253f0777991930513c8fa80d567
Raul Uhlmann <raulmu@outlook.com>
2022-02-19 15:07:09 -0300

    github pages integration

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
+import { authConfig } from '../../auth_config';
+
 export const environment = {
   production: true,
+  auth: {
+    ...authConfig,
+    redirectUri: 'https://raulmu.github.io/lembretes/',
+  },
 };
